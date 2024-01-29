5c9ded9a69f31b8e958bfbc34d4582c59384c7c7
iramogilnikova <iramogilnikova87@gmail.com>
2024-01-29 14:15:33 +0300

    исправила недочеты в 2.6

--- hw6.js
+++ hw6.js
@@ -18,9 +18,9 @@ for (let i = 0; i < arr.length; i++) {
 // Подсказка: Задачу можно решить методом перебора элементов или используя 
 // метод массива indexOf.
 
-const arr = [1, 5, 4, 10, 0, 3];
+const findPosition = [1, 5, 4, 10, 0, 3];
 
-console.log(arr.indexOf(4));
+console.log(findPosition.indexOf(4));
 
 // Задание 3
 // Дан массив чисел: [1, 3, 5, 10, 20].
@@ -34,7 +34,7 @@ console.log(numbers.join(' '));
 // С помощью вложенных циклов создайте многомерный массив вида: 
 // [[1, 1, 1], [1, 1, 1], [1, 1, 1]].
 
-const arr = [];
+const createArr = [];
 for (i = 0; i < 3; i++) {
     const nestArr = [];
     for (k = 0; k < 3; k++) {
@@ -42,16 +42,16 @@ for (i = 0; i < 3; i++) {
     }
     arr.push(nestArr);
 }
-console.log(arr);
+console.log(createArr);
 
 
 // Задание 5
 // Дан массив: [1, 1, 1].
 // Добавьте в конец массива значения 2, 2, 2.
 
-const arr = [1, 1, 1];
+const addValue = [1, 1, 1];
 arr.push(2, 2, 2);
-console.log(arr);
+console.log(addValue);
 
 // Задание 6
 // Дан массив: [9, 8, 7, 'a', 6, 5].
@@ -59,10 +59,9 @@ console.log(arr);
 // из данного массива. В результате работы программы вывести массив,
 // состоящий из цифр.
 
-const arr = [9, 8, 7, 'a', 6, 5];
-const result = arr.filter((el) => typeof el === 'number');
-let arrNumbs = result.sort();
-console.log(arrNumbs);
+const sortArr = [9, 8, 7, 'a', 6, 5];
+const result = sortArr.filter((el) => typeof el === 'number');
+console.log(result.sort());
 
 // Задание 7
 // Дан массив: [9, 8, 7, 6, 5].
@@ -70,15 +69,24 @@ console.log(arrNumbs);
 // которое ввёл пользователь, есть в массиве, вывести в alert «Угадал», в 
 // противном случае вывести «Не угадал».
 
-const arr = [9, 8, 7, 6, 5];
-let userAnswer = Number(prompt('Угадайте число'));
+const guessNumber = [9, 8, 7, 6, 5];
+let userAnswer = prompt('Угадайте число');
 
-if (isNaN(userAnswer)) {
-    console.log('nan')
-} else if (arr.includes(userAnswer)) {
-    alert('Угадал');
+if (userAnswer === null) {    
+  alert('Вы отменили ввод'); 
+} else if (userAnswer.trim() === '') {
+    alert('Строка пустая или состоит только из пробелов');
 } else {
-    alert('Не угадал');
+    let userNum = parseInt(userAnswer); 
+    if (isNaN(userNum)) {
+        alert('Вы ввели не число');
+    } else {
+        if (guessNumber.includes(userNum)) {
+            alert('Угадал');
+        } else {
+            alert('Не угадал');
+        }
+    }
 }
 
 // Задание 8
@@ -93,8 +101,8 @@ console.log(reverseStr);
 // Дан массив: [[1, 2, 3,],[4, 5, 6]].
 // Выведите в консоль массив вида: [1, 2, 3, 4, 5, 6].
 
-const arr = [[1, 2, 3,], [4, 5, 6]];
-const flatArr = arr.flat();
+const combineArr = [[1, 2, 3,], [4, 5, 6]];
+const flatArr = combineArr.flat();
 console.log(flatArr)
 
 // Задание 10
@@ -105,26 +113,24 @@ console.log(flatArr)
 // Обратите внимание, что у последнего элемента нет следующего.
 
 
-let arr = [1, 3, 5, 7, 9];
+let amountArr = [1, 3, 5, 7, 9];
 
-for (let i = 0; i < arr.length - 1; i++) {
-    let sumArr = arr[i] + arr[i + 1];
-    console.log(`Сумма текущего элемента равна ${arr[i]} и следуещего эелемента ${arr[i + 1]} = ${sumArr}`);
+for (let i = 0; i < amountArr.length - 1; i++) {
+    let sumArr = amountArr[i] + amountArr[i + 1];
+    console.log(`Сумма текущего элемента равна ${amountArr[i]} и следуещего эелемента ${amountArr[i + 1]} = ${sumArr}`);
 }
 
 // Задание 11
 // Создайте функцию, которая принимает на вход массив целых чисел, 
 // а возвращает массив квадратов этих чисел.
 
-function getSquare(arr) {
-    const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i] ** 2);
-    }
-    return result;
+let result = [9, 8, 7, 6, 5];
+
+function powArr(nums) {
+  return nums.map(num => num ** 2); 
 }
 
-console.log(getSquare([1, 3, 5, 7, 9]));
+console.log(powArr(result));
 
 
 // Задание 12
